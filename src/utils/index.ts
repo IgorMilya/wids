33afc4df7793d889ca2d2182ea60226651408e99
imilay11 <imilay11@gmail.com>
2026-01-05 00:29:22 +0200

    hot fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,5 @@ export {validationUser} from './validation'
 export {REGEX} from './regex'
 export {getDeviceId} from './deviceId'
 export {clearNetworkCache, saveNetworkCache, loadNetworkCache} from './cacheManager'
-export {initializeApp} from './appInit'
 export {enableGuestMode} from './guestMode'
 
